78d1e853f099c91852d066ba137f79eb4fc4a1e5
sa1amander83 <virom8@yandex.ru>
2021-01-08 21:48:41 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: path.resolve(__dirname, 'src/js/script.js'),
     output: {
@@ -10,30 +10,44 @@ module.exports = {
         filename: 'main.js'
     },
     mode: 'production',
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [new MiniCssExtractPlugin(),
+    new HTMLWebpackPlugin ({
+        template:'./src/faq.html',
+        filename: './dist/faq.html'
+    }),
+
+        new HTMLWebpackPlugin ({
+            template:'./src/index.html',
+            filename: './dist/index.html'
+        }),
+
+    ],
     module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(eot|woff|ttf|svg)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(jpg|jpeg|cur|png|bmp|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
+                   rules: [
+                {
+                    test: /\.scss$/i,
+                    use: ['style-loader', MiniCssExtractPlugin.loader,    'css-loader', 'sass-loader'],
 
 
-            }
-        ],
-    },
-    optimization: {
-        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    }
-};
+                },
+                {
+                    test: /\.(eot|woff|ttf|svg)$/,
+                    use: ['file-loader']
+                },
+                {
+                    test: /\.(jpg|jpeg|cur|png|bmp|gif)$/,
+                    use: ['file-loader']
+                },
+                {
+                    test: /\.html$/i,
+                    loader: 'html-loader',
+
+                }
+
+            ],
+        },
+        optimization: {
+            minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
+
+    };
